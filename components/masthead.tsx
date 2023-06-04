7d449d8995f238c28647a62b948fcaaf6ff2fccb
alexangria1992 <alex040892@gmail.com>
2023-06-04 20:07:49 +1200

    Masthead - Logo Animations

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -1,8 +1,9 @@
-import React, { useContext, useRef } from "react";
+import React, { useCallback, useContext, useRef, useState } from "react";
 import Image from "next/image";
 import { ScrollContext } from "../utils/scroll-observer";
 
 const Masthead: React.FC = () => {
+  const [imageLoaded, setImageLoaded] = useState(false);
   const refContainer = useRef<HTMLDivElement>(null);
   const { scrollY } = useContext(ScrollContext);
 
@@ -12,6 +13,10 @@ const Masthead: React.FC = () => {
   if (elContainer) {
     progress = Math.min(1, scrollY / elContainer.clientHeight);
   }
+
+  const handleImageLoaded = useCallback(() => {
+    setImageLoaded(true);
+  }, []);
 
   return (
     <div
@@ -30,7 +35,11 @@ const Masthead: React.FC = () => {
         <source src="/assets/masthead-bg.webm" type="video/webm; codecs=vp9" />
         <source src="/assets/masthead-bg.mp4" type="video/mp4" />
       </video>
-      <div className={`flex-grow-0 pt-10 transition-opacity duration-1000`}>
+      <div
+        className={`flex-grow-0 pt-10 transition-opacity duration-1000 ${
+          imageLoaded ? "opacity-100" : "opacity-0 "
+        }`}
+      >
         <Image
           className="absolute z-50"
           src="/assets/logo.svg"
@@ -48,13 +57,18 @@ const Masthead: React.FC = () => {
           <span>App Development,</span> <span>done right.</span>
         </h2>
       </div>
-      <div className="flex-grow-0 pb-20 mb:pb-10 transition-all duration-1000">
+      <div
+        className={`flex-grow-0 pb-20 mb:pb-10 transition-all duration-1000 ${
+          imageLoaded ? "opacity-100" : "opacity-0 -translate-y-10"
+        }`}
+      >
         <Image
           src="/assets/arrow-down.webp"
           width={188 / 3}
           height={105 / 3}
           alt="scroll-down"
           className="absolute z-50"
+          onLoad={handleImageLoaded}
         />
       </div>
     </div>
